abaf7eb0682e83499c56b0246577cfd10610b9c0
Abhinav Londhe <59164413+AbhinavLondhe8515@users.noreply.github.com>
2023-02-24 09:57:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,12 +77,12 @@ resource "azurerm_virtual_network" "this" {
   location            = each.value.location
   resource_group_name = each.value.resource_group
   
-
-  subnet {
-    name           = each.value.subnet_name
-    address_space {
+  address_space {
     address_prefixes = "10.0.1.0/24"
   }
+  
+  subnet {
+    name           = each.value.subnet_name
     address_prefix = each.value.subnet_address_prefix
     
   }
